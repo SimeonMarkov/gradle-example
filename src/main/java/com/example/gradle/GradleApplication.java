ba5d9c9b98c8e8dac15c157097cf35ce58d93af1
SimeonMarkov <simeon.markov1994@gmail.com>
2017-10-12 23:35:55 +0300

    comment main method temporarily

--- src/main/java/com/example/gradle/GradleApplication.java
+++ src/main/java/com/example/gradle/GradleApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GradleApplication.class, args);
+        //SpringApplication.run(GradleApplication.class, args);
     }
 }
